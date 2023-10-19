f27ac59e50042fefa07c0737b4fe281e522fb97e
Duy Anh <duyanhdo1010@gmail.com>
2023-10-19 21:19:01 +0700

    The game with fixed player input

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// function để lấy máy tính chọn random giá trị
 function getComputerChoice() {
     // function này sẽ trả về 1 trong 3 giá trị: 'rock', 'paper' hoặc 'scissors'
 
@@ -8,6 +9,34 @@ function getComputerChoice() {
     // trả về giá trị của biến lưu giá trị random đó
     return random;
 }
-// check giá trị trả về
-console.log(getComputerChoice());
+// check giá trị trả về của function getComputerChoice()
+//console.log(getComputerChoice());
 
+// function để chơi game và trả về kết quả của một round
+
+function playRound(playerSelection, computerSelection) {
+    // so sánh giá trị của hai tham số truyền vào và trả về đoạn text kết quả
+
+    if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection == `paper` && computerSelection == `rock`) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection == `paper` && computerSelection == `scissors`) {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection == `scissors` && computerSelection == `rock`) {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection == `scissors` && computerSelection == `paper`) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return 'Draw!';
+    }
+
+  }
+  
+// check giá trị trả về của function playRound()
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+  
