9a754039e4fc191c88c22e45f99bc5841ba499b8
Duy Anh <duyanhdo1010@gmail.com>
2023-10-19 22:01:16 +0700

    Complete my first console game

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ TRÒ CHƠI ROCK PAPER SCISSORS:
 Đây là trò chơi rock paper scissors hay còn gọi là oẳn tù tì nơi mà bạn sẽ thi đấu với máy. Trò chơi này có 3 lựa chọn là rock, paper và scissors. Bạn sẽ thắng nếu bạn chọn ra lựa chọn mà có thể thắng được lựa chọn của máy. Ví dụ như bạn chọn rock thì bạn sẽ thắng nếu máy chọn scissors. Nếu bạn chọn paper thì bạn sẽ thắng nếu máy chọn rock. Nếu bạn chọn scissors thì bạn sẽ thắng nếu máy chọn paper. Nếu bạn chọn lựa chọn giống nhau thì sẽ hòa.
 
 Thứ mà mình học được từ trò chơi này:
+
++ lấy được 1 items bất kỳ từ array
++ sử dụng các function để thực hiện từng nhiệm vụ của trò chơi
++ sử dụng if else để kiểm tra các trường hợp nhập vào
++ sử dụng while loop để lặp lại trò chơi và tìm người chiến thắng sau một khoảng lặp
--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+// cần 1 array để lưu ba giá trị (sử dụng biến toàn cục để dùng thêm ở dưới)
+var arr = ['rock', 'paper', 'scissors'];
+
 // function để lấy máy tính chọn random giá trị
 function getComputerChoice() {
     // function này sẽ trả về 1 trong 3 giá trị: 'rock', 'paper' hoặc 'scissors'
 
-    // cần 1 array để lưu ba giá trị
-    const arr = ['rock', 'paper', 'scissors'];
+
     // cần 1 biến để lưu giá trị random
     let random = arr[Math.floor(Math.random() * arr.length)];
     // trả về giá trị của biến lưu giá trị random đó
@@ -15,6 +17,10 @@ function getComputerChoice() {
 // function để chơi game và trả về kết quả của một round
 
 function playRound(playerSelection, computerSelection) {
+    // chuyển giá trị của tham số truyền vào thành chữ thường
+    playerSelection = playerSelection.toLowerCase();
+
+
     // so sánh giá trị của hai tham số truyền vào và trả về đoạn text kết quả
 
     if (playerSelection == 'rock' && computerSelection == 'paper') {
@@ -36,7 +42,55 @@ function playRound(playerSelection, computerSelection) {
   }
   
 // check giá trị trả về của function playRound()
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-  
+//   const playerSelection = "rock";
+//   const computerSelection = getComputerChoice();
+//   console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+
+    // biến đếm số lần thắng của player và computer
+    let playerWinCounter = 0;
+    let computerWinCounter = 0;
+
+    // chạy vòng lặp 5 lần để chơi game và in kết quả người thắng cuộc
+    let i = 0;
+    while (i < 5) {
+
+        // lấy giá trị của người dùng và máy tính
+        const computerSelection = getComputerChoice();
+        const playerSelection = prompt('Please enter your choice: rock, paper or scissors');
+
+
+        // kiểm tra đầu vào của người dùng
+        if (arr.includes(playerSelection.toLowerCase()) == false) {
+            alert('Please enter a valid choice!');
+            continue;
+        } else {
+            // xác định người chiến thăng mỗi round và tăng biến đếm lên 1
+            if (playRound(playerSelection, computerSelection) ==  `You Win! ${playerSelection} beats ${computerSelection}`){
+                alert("You win this round! Computer chose " + computerSelection);
+                playerWinCounter++;
+                i++;
+            } else if (playRound(playerSelection, computerSelection) == `You Lose! ${computerSelection} beats ${playerSelection}`) {
+                alert("You lose this round! Computer chose " + computerSelection);
+                computerWinCounter++;
+                i++;
+            } else {
+                alert("Draw! Computer chose " + computerSelection);
+                i++;
+            }
+        }
+    }
+    
+    // in kết quả người thắng cuộc
+    if (playerWinCounter > computerWinCounter) {
+        alert(`You win the game! with the score ${playerWinCounter} - ${computerWinCounter}`);
+    } else if (playerWinCounter < computerWinCounter){
+        alert(`You lose the game! ${playerWinCounter} - ${computerWinCounter}`);
+    } else {
+        alert(`Draw!`);
+    }
+
+}
+
+game();
